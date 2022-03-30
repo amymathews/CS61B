19618e652b2be080e4989313d411ff93e56888dd
Amy Mathews <amy.mathews0808@gmail.com>
2022-03-29 21:21:47 -0700

    I think I'm done?

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -10,9 +10,12 @@ class ECHashStringSet implements StringSet {
     private int ogBucketLength;
 
     public ECHashStringSet(){
+
         buckets = (LinkedList<String>[]) new LinkedList[5];
+        // loop through to make sure each index in a bucket
+        // is full of linked list.
         for (int i = 0; i < 5; i++) {
-            buckets[i] = new LinkedList<String>();
+            buckets[i] = new LinkedList<>();
         }
         ogBucketLength = 0;
     }
@@ -20,14 +23,16 @@ class ECHashStringSet implements StringSet {
     @Override
     public void put(String s) {
         // FIXME
-        // reach the load limit we resize.
-        if(buckets.length != 0 && (double) ogBucketLength/ (double) buckets.length > 5){
-            resize();
-        }
+        // reach the load limit we resize.Given in spec: resize the hash table
+        // as soon as the load factor exceeds 5
+//        if((double) ogBucketLength / (double) buckets.length > 5){
+//            resize();
+//        }
         int index = whichBucket(s);
         while (index > buckets.length) {
             resize();
         }
+        //get the linked list at buckets[index]
         if (buckets[index] == null) {
             buckets[index] = new LinkedList<>();
         }
@@ -52,6 +57,8 @@ class ECHashStringSet implements StringSet {
     }
 
     private int whichBucket(String s){
+        //figure out which index i bucket to go to
+        // get linked list
         int index = s.hashCode() % buckets.length;
         if (index < 0) {
             index = index & 0x7fffffff % buckets.length;
@@ -61,6 +68,7 @@ class ECHashStringSet implements StringSet {
     private void resize(){
         LinkedList<String>[] oldBucket = buckets;
         buckets = new LinkedList[2 * oldBucket.length];
+        // we need to rewrite the size.
         ogBucketLength = 0;
         for (LinkedList<String> l : oldBucket) {
             if (l != null) {
