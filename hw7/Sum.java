c3b1feccc09ad6507c3e30b05d500bd3322089c8
Amy Mathews <amy.mathews0808@gmail.com>
2022-04-12 16:41:46 -0700

    want to see the progress so far

--- hw7/Sum.java
+++ hw7/Sum.java
@@ -5,6 +5,13 @@ public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
+        for(int i = 0; i < A.length; i += 1) {
+            for(int j = 0; j < B.length; j += 1) {
+                    if(A[i]+ B[j] == m){
+                        return true;
+                    }
+            }
+        }
         return false;  // REPLACE WITH YOUR ANSWER
     }
 
