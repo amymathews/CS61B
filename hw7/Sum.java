d3fa42f93b90cca3013e18e94390b51c2528914e
Amy Mathews <amy.mathews0808@gmail.com>
2022-04-12 18:50:45 -0700

    want to see the progress so far

--- hw7/Sum.java
+++ hw7/Sum.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 /** HW #7, Two-sum problem.
  * @author
  */
@@ -7,12 +5,31 @@ public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
-        HashMap<Integer, Integer> reference  = new HashMap<>();
-        for(int i = 0; i < A.length; i += 1) {
-            reference.put(A[i], 0);
 
+        //sort A
+        for (int i = 1; i < A.length; i += 1) {
+            int key = A[i];
+            int j = i - 1;
+            while (j >= 0 && A[j] > key) {
+                A[j + 1] = A[j];
+                j = j - 1;
+            }
+            A[j + 1] = key;
+        }
+        //sort B
+        for (int i = 1; i < B.length; i += 1) {
+            int key = B[i];
+            int j = i - 1;
+            while (j >= 0 && B[j] > key) {
+                B[j + 1] = B[j];
+                j = j - 1;
+            }
+            B[j + 1] = key;
+        }
+
+        for(int i = 0; i < A.length; i += 1) {
             for(int j = 0; j < B.length; j += 1) {
-                    if(reference.containsKey(m-B[j])){
+                    if(A[i]+ B[j] == m){
                         return true;
                     }
             }
